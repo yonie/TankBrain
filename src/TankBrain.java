9642043243ebcf3be0d0e29cc3d7ddd4e2589104
yonie <yonie@yonie.org>
2013-02-09 23:19:30 +0100

    doc

--- src/TankBrain.java
+++ src/TankBrain.java
@@ -1,10 +1,21 @@
 public class TankBrain {
 
+	// the remote host at which the server resides
 	static final String remoteHost = "localhost";
+	
+	// the port to use for downstream communication
 	static final int downstreamPort = 1360;
+	
+	// the port to use for upstream communication
 	static final int upstreamPort = 1359;
+	
+	// version string 
 	static final String versionString = "0.1.0";
+	
+	// the user name to log on to the system
 	static final String userName = "Ronald";
+	
+	// tank color
 	static final int tankColorRed = 20;
 	static final int tankColorGreen = 20;
 	static final int tankColorBlue = 20;
