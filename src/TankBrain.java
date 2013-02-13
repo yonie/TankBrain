5012a5fe956ef1ea13eec616cb136ad43ee6fd1d
yonie <yonie@yonie.org>
2013-02-13 11:50:59 +0100

    ctrl+shift+f

--- src/TankBrain.java
+++ src/TankBrain.java
@@ -2,21 +2,21 @@ public class TankBrain {
 
 	// the remote host at which the server resides
 	static final String remoteHost = "localhost";
-	
+
 	// the port to use for downstream communication
 	static final int downstreamPort = 1360;
-	
+
 	// the port to use for upstream communication
 	static final int upstreamPort = 1359;
-	
-	// version string 
+
+	// version string
 	static final int majorVersion = 0;
 	static final int minorVersion = 2;
 	static final int revisionVersion = 0;
-	
+
 	// the user name to log on to the system
 	static final String userName = "Ronald";
-	
+
 	// tank colors
 	static final int tankColorRed = 20;
 	static final int tankColorGreen = 20;
@@ -25,8 +25,8 @@ public class TankBrain {
 	public static void main(String[] args) {
 		System.out.println("DEBUG: Begin...");
 		Dispatcher dispatcher = new Dispatcher(remoteHost, downstreamPort,
-				upstreamPort, majorVersion, minorVersion, revisionVersion, userName, tankColorRed,
-				tankColorGreen, tankColorBlue);
+				upstreamPort, majorVersion, minorVersion, revisionVersion,
+				userName, tankColorRed, tankColorGreen, tankColorBlue);
 		System.out.println("DEBUG: End.");
 	}
 }
