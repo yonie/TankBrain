ded7e50afcdbd29b079c2e34372d9627ffb2c3c9
yonie <yonie@yonie.org>
2013-02-27 11:07:47 +0100

    implemented log level

--- src/org/yoniehax/helper/QuickLog.java
+++ src/org/yoniehax/helper/QuickLog.java
@@ -8,6 +8,8 @@ import java.util.Date;
  */
 public class QuickLog {
 
+	final static int logLevel = 2;
+	
 	static int DEBUG = 1;
 	static int INFO = 2;
 	static int ERROR = 3;
@@ -46,10 +48,12 @@ public class QuickLog {
 	 * @param errorLevel
 	 */
 	private static void log(String s, int errorLevel) {
-		System.out.print(DateFormat.getInstance().format(new Date().getTime()) + " ");
-		System.out.print((errorLevel == INFO ? "INFO" : (errorLevel == DEBUG ? "DEBUG" : "ERROR")) + ": ");
-		System.out.print(s);
-		System.out.println();
+		if (errorLevel >= logLevel) {
+			System.out.print(DateFormat.getInstance().format(new Date().getTime()) + " ");
+			System.out.print((errorLevel == INFO ? "INFO" : (errorLevel == DEBUG ? "DEBUG" : "ERROR")) + ": ");
+			System.out.print(s);
+			System.out.println();
+		}
 	}
 
 }
