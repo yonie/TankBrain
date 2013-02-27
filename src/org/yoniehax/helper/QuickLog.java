bd32f5d4419bebc670cddf6ad2d6d943c7bea77e
yonie <yonie@yonie.org>
2013-02-27 14:33:28 +0100

    doc

--- src/org/yoniehax/helper/QuickLog.java
+++ src/org/yoniehax/helper/QuickLog.java
@@ -3,51 +3,53 @@ package org.yoniehax.helper;
 import java.text.DateFormat;
 import java.util.Date;
 
-/*
- * 
+/**
+ * The QuickLog class is used to provide basic log level functionality. 
  */
 public class QuickLog {
 
-	final static int logLevel = 2;
-	
+	// current log level to display, lower is more verbose
+	static int logLevel = 2;
+
 	static int DEBUG = 1;
 	static int INFO = 2;
 	static int ERROR = 3;
 
 	/**
+	 * Logs text with 'info' log level.
 	 * 
-	 * 
-	 * @param s
+	 * @param text
+	 *            text to log.
 	 */
-	public static void info(String s) {
-		QuickLog.log(s, INFO);
+	public static void info(String text) {
+		QuickLog.log(text, INFO);
 	}
 
 	/**
+	 * Logs text with 'debug' log level.
 	 * 
-	 * 
-	 * @param s
+	 * @param text
+	 *            text to log.
 	 */
-	public static void debug(String s) {
-		QuickLog.log(s, DEBUG);
+	public static void debug(String text) {
+		QuickLog.log(text, DEBUG);
 	}
 
 	/**
+	 * Logs text with 'error' log level.
 	 * 
-	 * 
-	 * @param s
+	 * @param text
+	 *            text to log.
 	 */
-	public static void error(String s) {
-		QuickLog.log(s, ERROR);
+	public static void error(String text) {
+		QuickLog.log(text, ERROR);
 	}
 
-	/**
-	 * 
-	 * 
-	 * @param s
-	 * @param errorLevel
+	/*
+	 * Logs given text with error level
 	 */
 	private static void log(String s, int errorLevel) {
+		// only log when error level is sufficient
 		if (errorLevel >= logLevel) {
 			System.out.print(DateFormat.getInstance().format(new Date().getTime()) + " ");
 			System.out.print((errorLevel == INFO ? "INFO" : (errorLevel == DEBUG ? "DEBUG" : "ERROR")) + ": ");
